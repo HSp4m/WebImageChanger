6e5275985ccecf6056301392b2b2b4763e65e57e
HSp4m <sp4m787@gmail.com>
2023-07-31 19:51:43 -0400

    0.0.1 (Now working)

--- main/content.js
+++ main/content.js
@@ -2,20 +2,22 @@ document.addEventListener("DOMContentLoaded", function() {
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
       const activeTab = tabs[0];
       document.getElementById("btn").addEventListener("click", () => {
+        const newimage = document.getElementById("URLInput").value;
         chrome.scripting.executeScript({
           target: { tabId: activeTab.id },
-          func: () => {
+          func: (newimage) => {
             const images = document.querySelectorAll("img");
-            const newimage = document.getElementById("input").value;
+         
             console.log(newimage);
             images.forEach((img) => {
              // img.remove();
             if(newimage !== null) {
                 img.src = newimage;
-            }
+            } 
               
             });
           },
+          args: [newimage], 
         });
       });
     });
